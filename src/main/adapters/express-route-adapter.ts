ce20879013e04abc0dcc3630b1960f936fcba65d
Luiz <luizz.ggr@gmail.com>
2022-04-07 16:16:59 -0300

    refactor: making route adapter more generic

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,8 +1,7 @@
-import { ShowCasesByDayController } from '@/application/controller/show-case-by-day';
 import { IHttpRequest } from '@/application/helpers';
 import { Request, Response } from 'express';
 
-export const adaptRoute = (controller: ShowCasesByDayController) => async (req: Request, res: Response) => {
+export const adaptRoute = (controller: any) => async (req: Request, res: Response) => {
   const httpRequest: IHttpRequest = {
     data: req.params.date,
   };
