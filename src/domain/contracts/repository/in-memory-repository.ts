b6dd602180846aed23cc87539f00e3a16f0e51f9
Luiz <luizz.ggr@gmail.com>
2022-04-07 10:46:44 -0300

    feat: should return a date equal to date giving by a user

--- src/domain/contracts/repository/in-memory-repository.ts
+++ src/domain/contracts/repository/in-memory-repository.ts
@@ -4,13 +4,13 @@ import { ICovidVariantsRepository } from '@/domain/contracts/repository';
 export class InMemoryRepository implements ICovidVariantsRepository {
   constructor(private readonly repo: ICovidVariantsDTO[] = []) {}
 
-  async showDates(date: Date): Promise<ICovidVariantsDTO[]> {
-    const validDates = await this.repo.filter(covidDate => covidDate.date === date);
-    return validDates;
-  }
-
   async showAllDailyCases(day: number): Promise<ICovidVariantsDTO[]> {
     const allCases = await this.repo.filter(covidCase => covidCase.date.getDate() === day - 1);
     return allCases;
+  }
+
+  async showByDates(date: Date): Promise<ICovidVariantsDTO[]> {
+    const validDates = await this.repo.filter(covidDate => covidDate.date === date);
+    return validDates;
   }
 }
