2c154316932ad00ec689d144f5f83453f4c06d29
Luiz <luizz.ggr@gmail.com>
2022-04-07 14:19:11 -0300

    chore: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    roots: ['<rootDir>/test'],
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.ts',
-      '!**/test/**',
-      '!**/config/**',
-    ],
-    testEnvironment: 'node',
-    preset: 'ts-jest',
-    transform: {
-      '.+\\.ts$': 'ts-jest',
-    },
-  
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-      '^@test/(.*)$': '<rootDir>/test/$1',
-    },
-  };
+  roots: ['<rootDir>/test'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**',
+  ],
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+};
