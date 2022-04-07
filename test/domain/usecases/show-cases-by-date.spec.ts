1ff67b4c91c9f54fe2602c89b3b2ab7a7fb54fc3
Luiz <luizz.ggr@gmail.com>
2022-04-07 10:47:33 -0300

    test: should return a date equal to date giving by a user (remove comments)

---
+++ test/domain/usecases/show-cases-by-date.spec.ts
@@ -0,0 +1,39 @@
+// import { ICovidVariantsDTO, IUseCase } from '@/domain/contracts/gateways';
+// import { ShowCasesByDate } from '@/domain/usecases/show-cases-by-date';
+// import { InMemoryRepository } from '@test/domain/contracts/repository/in-memory-repository';
+
+describe('show cases by date use case', () => {
+//   const covidCases: ICovidVariantsDTO[] = [{
+//     location: 'Angola',
+//     date: new Date('2020-10-12'),
+//     variant: 'Iota',
+//     num_sequences: 0,
+//     perc_sequences: 0.0,
+//     num_sequences_total: 29,
+//   },
+//   {
+//     location: 'Angola',
+//     date: new Date('2020-10-12'),
+//     variant: 'Kappa',
+//     num_sequences: 0,
+//     perc_sequences: 0.0,
+//     num_sequences_total: 29,
+//   },
+//   {
+//     location: 'Angola',
+//     date: new Date('2020-10-15'),
+//     variant: 'Lambda',
+//     num_sequences: 0,
+//     perc_sequences: 0.0,
+//     num_sequences_total: 29,
+//   }];
+
+  test('should return all cases in 20-10-12', async () => {
+    console.log(3);
+
+    //     const repo = new InMemoryRepository(covidCases);
+    //     const useCase: IUseCase = new ShowCasesByDate(repo);
+    //     const result: ICovidVariantsDTO[] = await useCase.perform('2020-10-15');
+    //     expect(result).toEqual(covidCases[0].date);
+  });
+});
