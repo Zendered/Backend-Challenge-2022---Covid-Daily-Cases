aa4aa86e3d9dc107aca34433e7058cdcf3a953ed
Luiz <luizz.ggr@gmail.com>
2022-04-08 15:56:35 -0300

    feat: implementing function to return all cases before some date

--- src/domain/contracts/repository/in-memory-repository.ts
+++ src/domain/contracts/repository/in-memory-repository.ts
@@ -1,6 +1,6 @@
 import { ICovidVariantsDTO } from '@/domain/contracts/gateways/covid-variants-dto';
 import { ICovidVariantsRepository } from '@/domain/contracts/repository';
-import { isSameDay } from 'date-fns';
+import { isSameDay, isBefore } from 'date-fns';
 
 export class InMemoryRepository implements ICovidVariantsRepository {
   constructor(private readonly repo: ICovidVariantsDTO[] = []) {}
@@ -10,8 +10,10 @@ export class InMemoryRepository implements ICovidVariantsRepository {
     return allCases;
   }
 
-  async showByDates(date: Date): Promise<ICovidVariantsDTO[]> {
-    const validDates = await this.repo.filter(covidDate => isSameDay(covidDate.date, date));
-    return validDates;
+  async ShowCasesUntilNow(date: Date): Promise<ICovidVariantsDTO[]> {
+    const covidCase = await this.showCasesOfTheDay(date);
+    const validDates = await this.repo.filter(covidDate => isBefore(covidDate.date, date));
+    const result = [...covidCase, ...validDates];
+    return result;
   }
 }
