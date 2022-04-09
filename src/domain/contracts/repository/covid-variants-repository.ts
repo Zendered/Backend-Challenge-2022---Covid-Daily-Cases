3416a698c367d0fabc3acdde6f28898ce491bf59
Luiz <luizz.ggr@gmail.com>
2022-04-09 13:31:14 -0300

    feat: interfaces to import all available dates

--- src/domain/contracts/repository/covid-variants-repository.ts
+++ src/domain/contracts/repository/covid-variants-repository.ts
@@ -3,4 +3,5 @@ import { ICovidVariantsDTO } from '../gateways/covid-variants-dto';
 export interface ICovidVariantsRepository {
     ShowCasesUntilNow(date: Date): Promise<ICovidVariantsDTO[]>
     showCasesOfTheDay(date: Date): Promise<ICovidVariantsDTO[]>
+    allAvailableDate(): Promise<ICovidVariantsDTO[]>
 }
