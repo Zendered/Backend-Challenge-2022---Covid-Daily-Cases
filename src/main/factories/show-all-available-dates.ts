1c3c2a2f5bfa8486b2bca6a433e994bf10d66939
Luiz <luizz.ggr@gmail.com>
2022-04-10 09:02:54 -0300

    feat: routes config

--- src/main/factories/show-all-available-dates.ts
+++ src/main/factories/show-all-available-dates.ts
@@ -1,75 +1,9 @@
-import { ICovidVariantsDTO } from '@/domain/contracts/gateways';
-import { InMemoryRepository } from '@/domain/contracts/repository/in-memory-repository';
 import { AllAvailableDateController } from '@/application/controller/show-all-available-dates';
 import { AllAvailableDate } from '@/domain/usecases/show-all-available-dates';
+import { MongoDbRepository } from '@/infra/repository/mongodb';
 
 export const MakeShowAllAvailableDatesController = (): AllAvailableDateController => {
-  const covidVariants: ICovidVariantsDTO[] = [{
-    location: 'Angola',
-    date: new Date('2020-10-12'),
-    variant: 'Iota',
-    num_sequences: 0,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'Angola',
-    date: new Date('2020-10-12'),
-    variant: 'Iota',
-    num_sequences: 1,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'Angola',
-    date: new Date('2020-10-12'),
-    variant: 'Iota',
-    num_sequences: 0,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'Angola',
-    date: new Date('2020-10-12'),
-    variant: 'Omicron',
-    num_sequences: 0,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'Angola',
-    date: new Date('2020-10-12'),
-    variant: 'Omicron',
-    num_sequences: 0,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'France',
-    date: new Date('2020-10-17'),
-    variant: 'Omicron',
-    num_sequences: 0,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'Brazil',
-    date: new Date('2020-10-10'),
-    variant: 'Kappa',
-    num_sequences: 7,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  },
-  {
-    location: 'Brazil',
-    date: new Date('2020-10-10'),
-    variant: 'Kappa',
-    num_sequences: 9,
-    perc_sequences: 0.0,
-    num_sequences_total: 1,
-  }];
-
-  const inMemoryRepository = new InMemoryRepository(covidVariants);
+  const inMemoryRepository = new MongoDbRepository();
   const usecase = new AllAvailableDate(inMemoryRepository);
   const controller = new AllAvailableDateController(usecase);
   return controller;
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,10 @@
 import 'module-alias/register';
-import { app } from './config/app';
+import 'dotenv/config';
+import { MongoHelper } from '@/infra/repository/mongodb/helper';
 
-app.listen(3333, () => {
-  console.log('server on!');
-});
+MongoHelper.connect(`${process.env.PROD_MONGO_URL}`)
+  .then(async () => {
+    const { app } = await import('@/main/config/app');
+    app.listen(3333, () => console.log('Server running'));
+  })
+  .catch(console.error);
