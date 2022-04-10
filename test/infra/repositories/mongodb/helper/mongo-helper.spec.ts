aec4fd7d8df948e795e8525c0a3ac9d9ed3be8c4
Luiz <luizz.ggr@gmail.com>
2022-04-10 09:04:09 -0300

    test: test the functions in the mongodbRepository

---
+++ test/infra/repositories/mongodb/helper/mongo-helper.spec.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import { MongoHelper } from '@/infra/repository/mongodb/helper';
+import { MongoDbRepository } from '@/infra/repository/mongodb';
+
+describe('mongodb covid cases repository', () => {
+  jest.setTimeout(45000);
+  beforeAll(async () => {
+    await MongoHelper.connect(`${process.env.PROD_MONGO_URL}`);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('should return cases of the day', async () => {
+    console.log('');
+
+    const repo = new MongoDbRepository();
+    const result = await repo.showCasesOfTheDay(new Date('2020-10-12'));
+
+    expect(result).toBeTruthy();
+  });
+
+  test('should return cases until now', async () => {
+    const repo = new MongoDbRepository();
+    const result = await repo.ShowCasesUntilNow(new Date('2020-10-17'));
+
+    expect(result).toBeTruthy();
+  });
+
+  test('should return all available dates', async () => {
+    const repo = new MongoDbRepository();
+    const result = await repo.allAvailableDate();
+
+    expect(result).toBeTruthy();
+  });
+});
