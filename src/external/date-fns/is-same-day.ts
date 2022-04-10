6bdf4af06efd2cce441574940aa43c5d226b67a6
Luiz <luizz.ggr@gmail.com>
2022-04-10 09:00:15 -0300

    feat: return true if the entire date is equal to the date given by the user

--- src/external/date-fns/is-same-day.ts
+++ src/external/date-fns/is-same-day.ts
@@ -3,9 +3,9 @@ import { isSameDay, isSameMonth, isSameYear } from 'date-fns';
 
 export class CompareDate implements ICompareDate {
   isSameDate(dateNow: any, DateToCompare: Date): boolean {
-    const sameDay = isSameDay(dateNow, DateToCompare);
-    const sameMonth = isSameMonth(dateNow, DateToCompare);
-    const sameYear = isSameYear(dateNow, DateToCompare);
+    const sameDay = isSameDay(new Date(dateNow), new Date(DateToCompare));
+    const sameMonth = isSameMonth(new Date(dateNow), new Date(DateToCompare));
+    const sameYear = isSameYear(new Date(dateNow), new Date(DateToCompare));
     if (sameDay && sameMonth && sameYear) {
       return true;
     }
