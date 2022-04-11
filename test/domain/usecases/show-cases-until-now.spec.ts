e768e07d03a0df3a76c5a248516a704b7e24faa3
Luiz <luizz.ggr@gmail.com>
2022-04-11 13:55:56 -0300

    test: sum all cases happened in the given date

--- test/domain/usecases/show-cases-until-now.spec.ts
+++ test/domain/usecases/show-cases-until-now.spec.ts
@@ -5,43 +5,43 @@ import { InMemoryRepository } from '@test/domain/contracts/repository/in-memory-
 describe('show cases by date use case', () => {
   const covidCases: ICovidVariantsDTO[] = [{
     location: 'Angola',
-    date: new Date('2020-10-12'),
+    date: new Date('2020-10-02'),
     variant: 'Iota',
     num_sequences: 0,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 20,
   },
   {
     location: 'Angola',
-    date: new Date('2020-10-12'),
+    date: new Date('2020-10-07'),
     variant: 'Iota',
     num_sequences: 1,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 29,
   },
   {
     location: 'Angola',
-    date: new Date('2020-10-12'),
+    date: new Date('2020-10-16'),
     variant: 'Iota',
     num_sequences: 0,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 20,
   },
   {
-    location: 'Angola',
-    date: new Date('2020-10-12'),
+    location: 'Argentina',
+    date: new Date('2020-10-07'),
     variant: 'Omicron',
     num_sequences: 0,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 59,
   },
   {
-    location: 'Angola',
-    date: new Date('2020-10-12'),
+    location: 'Argentina',
+    date: new Date('2020-10-11'),
     variant: 'Omicron',
     num_sequences: 0,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 10,
   },
   {
     location: 'France',
@@ -49,38 +49,32 @@ describe('show cases by date use case', () => {
     variant: 'Omicron',
     num_sequences: 0,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 7,
   },
   {
     location: 'Brazil',
-    date: new Date('2020-10-10'),
+    date: new Date('2020-10-14'),
     variant: 'Kappa',
     num_sequences: 7,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
+    num_sequences_total: 30,
   },
   {
     location: 'Brazil',
-    date: new Date('2020-10-10'),
+    date: new Date('2020-10-12'),
     variant: 'Kappa',
     num_sequences: 9,
     perc_sequences: 0.0,
-    num_sequences_total: 1,
-  }];
+    num_sequences_total: 39,
+  },
+  ];
+  const repo = new InMemoryRepository(covidCases);
+  const useCase: IUseCase = new ShowCasesUntilNow(repo);
 
   test('should return all cases until 20-10-17', async () => {
-    const repo = new InMemoryRepository(covidCases);
-    const useCase: IUseCase = new ShowCasesUntilNow(repo);
     const result: ICovidVariantsDTO[] = await useCase.perform('2020-10-17');
     expect(result[0].location).toEqual(covidCases[5].location);
     expect(result[1].location).toEqual(covidCases[0].location);
-    expect(result[2].location).toEqual(covidCases[6].location);
+    // expect(result[2].location).toEqual(covidCases[6].location);
   });
-
-  // test('should return all cases in 20-10-15', async () => {
-  //   const repo = new InMemoryRepository(covidCases);
-  //   const useCase: IUseCase = new ShowCasesByDate(repo);
-  //   const result: ICovidVariantsDTO[] = await useCase.perform('2020-10-15');
-  //   expect(result[0].date).toEqual(covidCases[2].date);
-  // });
 });
