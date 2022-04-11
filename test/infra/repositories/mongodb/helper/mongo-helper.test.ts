ee10c23b96424fb35884eb9789a8df971c815f5a
Luiz <luizz.ggr@gmail.com>
2022-04-11 19:13:00 -0300

    test: testing mongodb connection

--- test/infra/repositories/mongodb/helper/mongo-helper.test.ts
+++ test/infra/repositories/mongodb/helper/mongo-helper.test.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import { MongoHelper } from '@/infra/repository/mongodb/helper';
 import { MongoDbRepository } from '@/infra/repository/mongodb';
+import { ShowCasesUntilNow } from '@/domain/usecases/show-cases-until-now';
+import { ShowCasesUntilNowController } from '@/application/controller/show-case-until-now';
+import { IHttpRequest, IHttpResponse } from '@/application/helpers';
+import { ICasesOfTheDay } from '@/domain/contracts/gateways/cases-of-the-day';
 
 describe('mongodb covid cases repository', () => {
   jest.setTimeout(45000);
@@ -12,7 +16,7 @@ describe('mongodb covid cases repository', () => {
     await MongoHelper.disconnect();
   });
 
-  test.only('should return cases of the day', async () => {
+  test('should return cases of the day', async () => {
     const repo = new MongoDbRepository();
     const result = await repo.showCasesOfTheDay(new Date('2020-10-12'));
 
@@ -21,13 +25,18 @@ describe('mongodb covid cases repository', () => {
 
   test('should return cases until now', async () => {
     const repo = new MongoDbRepository();
-    const result = await repo.ShowCasesUntilNow(new Date('2020-10-17'));
+    const useCase = new ShowCasesUntilNow(repo);
+    const controller = new ShowCasesUntilNowController(useCase);
+    const request: IHttpRequest = {
+      data: '2020-09-28',
+    };
+    const result: IHttpResponse<ICasesOfTheDay[]> = await controller.handle(request.data);
 
     expect(result).toBeTruthy();
   });
 
   test('should return all available dates', async () => {
-    const repo = new MongoDbRepository();
+    const repo = await new MongoDbRepository();
     const result = await repo.allAvailableDate();
 
     expect(result).toBeTruthy();
