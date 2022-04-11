6ec5eb03255370ea011419f6b532c9389c37ae21
Luiz <luizz.ggr@gmail.com>
2022-04-11 13:55:22 -0300

    fix: sum all cases happened in the given date

--- src/domain/usecases/show-cases-until-now.ts
+++ src/domain/usecases/show-cases-until-now.ts
@@ -1,3 +1,4 @@
+import { ICovidVariantsDTO } from '../contracts/gateways';
 import { ICasesOfTheDay } from '../contracts/gateways/cases-of-the-day';
 import { IUseCase } from '../contracts/gateways/use-case';
 import { ICovidVariantsRepository } from '../contracts/repository';
@@ -8,20 +9,21 @@ export class ShowCasesUntilNow implements IUseCase {
   async perform(date: string): Promise<ICasesOfTheDay[]> {
     const caseDate = new Date(date);
     const value = await this.repo.ShowCasesUntilNow(caseDate);
-    const result:ICasesOfTheDay[] = value.reduce<ICasesOfTheDay[]>((acc, covidCase):ICasesOfTheDay[] => {
+    const result:ICasesOfTheDay[] = value.reduce<ICasesOfTheDay[]>((acc, covidCase:ICovidVariantsDTO):ICasesOfTheDay[] => {
+      // descobre se a pais do "acc" findCountry é igual a do covidCase
       const findCountry = acc.find(tracked => tracked.location === covidCase.location);
 
       if (findCountry) {
+        // descobre se a variante do findCountry é igual a do covidCase
         const findVariant = findCountry.variant.find(variant => variant.name === covidCase.variant);
-        const findValue = findCountry.variant.find(totalCases => totalCases.value === covidCase.num_sequences_total);
 
-        if (findVariant) {
-          findVariant.value += 1;
+        if (findVariant && findCountry.location === acc[acc.length - 1].location && findCountry.variant[findCountry.variant.length - 1].name === acc[acc.length - 1].variant[findCountry.variant.length - 1].name) {
+          findVariant.value += covidCase.num_sequences_total;
           return acc;
         }
 
-        if (findValue && findCountry.location === acc[0].location && findCountry.variant[0].name === acc[0].variant[0].name) {
-          findValue.value += acc[0].variant[0].value;
+        if (findVariant) {
+          findVariant.value += 1;
           return acc;
         }
 
